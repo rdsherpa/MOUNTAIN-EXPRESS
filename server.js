b23f7c7fab99255844a1e069fa28e410b56a97eb
Julian Betts <wjulianbetts@gmail.com>
2022-11-25 14:23:16 -0800

    cookies, handlebar in  server.js

--- server.js
+++ server.js
@@ -11,7 +11,33 @@ const PORT = process.env.PORT || 3001;
 
 // need to set up cookeies, secret
 
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
 // const helpers
+
+const hbs = exphbs.create({ /*helpers*/ });
+
+
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // const hbs
 
